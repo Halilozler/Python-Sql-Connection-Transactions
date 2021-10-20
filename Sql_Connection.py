f44cf580ee9f82c2f0eb599545572762966b89d2
Halil Özler <45699509+Halilozler@users.noreply.github.com>
2021-10-20 16:20:48 +0300

    Add files via upload

---
+++ Sql_Connection.py
@@ -0,0 +1,36 @@
+import pyodbc
+
+def sql_connection():
+    """
+    DRIVER:
+        for drivers in pyodbc.drivers():
+            print(drivers)
+        Komutu ile yazan driver adlarından biridir. Genelde üçünden biri olur:
+        {SQL Server}
+        {SQL Server Native Client 11.0}
+        {ODBC Driver 17 for SQL Server}
+
+    SERVER:
+        Serverimizin ismi.
+
+    DATABASE:
+        Erişmek istediğimiz veritabanı.
+
+    Trusted_Connection=yes
+        Eğer serverimizin erişiminde şifre yok ise direk bağlanmamızı sağşar
+
+    Eğer kullanıcı adı ve şifremiz var ise:
+    UID=kullanıcı_adı;
+    PWD=şifre;
+
+    """
+    driver = "{ODBC Driver 17 for SQL Server}"
+    server = "server_name"
+    database = "Animals"
+
+    connection = pyodbc.connect(
+        f'DRIVER={driver}; SERVER={server}; DATABASE={database}; Trusted_Connection=yes'
+    )
+
+    cursor = connection.cursor()
+    return cursor
---
+++ main.py
@@ -0,0 +1,13 @@
+import Sql_Transactions
+
+#Ekleme: Table ismi, Column isimleri, (değerler) sırası ile
+#Sql_Transactions.Add("tbl_Dog","Dog_Name,Dog_Age",("cesper",8))
+
+#Silme: Silinecek Id, Table ismi
+#Sql_Transactions.Delete(2,"tbl_Dog")
+
+#Güncelleme:primery Key, table, Değiştirmek istediğin kolonun ismi, Değer.
+#Sql_Transactions.Update(3,"tbl_Dog","Dog_Age","5")
+
+
+
